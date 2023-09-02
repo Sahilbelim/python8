b10105de6448ff57a6b8b9a7c16f7e59a737f48b
Sahilbelim <106684275+Sahilbelim@users.noreply.github.com>
2023-09-02 20:04:31 +0530

    list

---
+++ lesson16.py
@@ -0,0 +1,12 @@
+name="Belim sahil \n"
+    # 01234567890
+print(name)
+print(name[0])
+print(name[6])
+print(name[0:5])
+print(name[6:])
+
+print(name * 5)
+
+print("Name : " + name)
+# name[0]="h" not work 
---
+++ list1.py
@@ -0,0 +1,15 @@
+name=["sahil","harsh \n","param","darshan","Vasu"]
+list1=[45.36,25,"belim",85,"patel"]
+print(name)
+print(list1)
+
+print(name[0])
+print(name[1] *5)
+print(name[2])
+print(name[3])
+print(name[4])
+
+print("Weight : ",list1[0])
+print(list1[1:3])
+print(list1[2:])
+print(list1+name)
