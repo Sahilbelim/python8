5ee80340d0b4f492d76d7a671caaba4a434244c0
Sahilbelim <106684275+Sahilbelim@users.noreply.github.com>
2023-10-21 19:16:23 +0530

    time stamp

---
+++ datetime/my_datetime.py
@@ -0,0 +1,10 @@
+
+from datetime import datetime
+
+currentTimeStamp =datetime.now().timestamp()
+
+print(currentTimeStamp) #1970 jan 1
+
+currentTimeStamp=datetime(2005,5,30).timestamp() # 1970 to 2005
+
+print(currentTimeStamp) #1970 jan 1 # 2005 to 2023
