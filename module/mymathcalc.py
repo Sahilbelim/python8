db7d611979f1dda5faac9a4e0fb4e6a8c08fbadc
Sahilbelim <106684275+Sahilbelim@users.noreply.github.com>
2023-12-05 20:23:13 +0530

    math

---
+++ module/mymathcalc.py
@@ -0,0 +1,26 @@
+import math
+
+print(math.modf(7/4))
+
+print(math.trunc(13.45963))
+
+print(math.exp(3))
+
+# e**2
+# e=2.718
+
+print(math.expm1(3))
+
+print(math.log(2,10))
+print(math.log1p(2))
+print(math.log2(2))
+print(math.log10(2))
+
+print(math.pow(2,3))
+
+print(math.sqrt(36))
+
+print(math.acos(0.5)*((180*7)/22))
+print(math.sin(0.5)*((180/(22/7))))
+
+# e**3-1
