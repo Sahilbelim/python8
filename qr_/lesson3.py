3952200cb3dc2c9f675c81e370306792c8b57f4b
Sahilbelim <106684275+Sahilbelim@users.noreply.github.com>
2023-12-18 20:10:44 +0530

    qr code

--- qr_/lesson3.py
+++ qr_/lesson3.py
@@ -1,4 +1,5 @@
 import qrcode
+from qrcode.image.styledpil import StyledPilImage
 qr=qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -9,8 +10,10 @@ qr=qrcode.QRCode(
 qr.add_data("https://github.com/Sahilbelim/python8/blob/main/qr_/lesson2.py")
 qr.make(fit=True)
 
-img=qr.make_image(fill_color="#190482", back_color="#C2D9FF")
+# img=qr.make_image(fill_color="#190482", back_color="#C2D9FF")
+
+img = qr.make_image(image_factory=StyledPilImage, embeded_image_path="./py8_logo1.png")
 
 type(img)
 
-img.save("color_qr2.png")
+img.save("color_qr3.png")
